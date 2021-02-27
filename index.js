b9561fcebf27f55fc45461c4b286d053176c2860
danivi95 <danvitale95@gmail.com>
2021-02-27 16:32:50 +0100

    implementazione download file CSV

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Importo le librerie di Express, ovvero un framework che fornisce funzionalità per sviluppare WebApp
 const express = require('express');
+// Importo le query
+const Records = require("./query");
+// Importo Json2csv che permette di convertire JSON in CSV
+const Json2csvParser = require('json2csv').Parser;
+
 // Creo un web server
 const server = express();
 // Configuro la porta. Vado a leggere la variabile d’ambiente PORT e, se c’è un valore definito, allora prende tale valore, altrimenti setta la porta 4000
@@ -8,9 +13,6 @@ const port = process.env.PORT || '4000';
 server.listen(port, () => {
     console.log('Server running on http://localhost:' + port);
 });
-
-// Importo le query
-const Records = require("./query");
 
 // GET endpoint per la lista dei record denormalizzati
 server.get("/record-denormalizzati/", (req, res) => {
@@ -23,9 +25,33 @@ server.get("/record-denormalizzati/", (req, res) => {
         const startIndex = (page - 1) * offset;
         const endIndex = page * offset;
 
+        // Ritorna la copia di una porzione dell'array contenente gli elementi compresi tra startIndex e endIndex (escluso)
         const results = records.slice(startIndex, endIndex);
 
         // Ritorna i json come risultati
         res.status(200).json(results);
+    })
+});
+
+// GET endpoint per il download della lista dei record denormalizzati
+server.get("/download/", (req, res) => {
+    Records.findAll()
+    .then(records => {
+        // Converto i record JavaScript in una stringa JSON
+        const jsonRecords = JSON.parse(JSON.stringify(records));
+        // Definisco i campi del file CSV
+        const csvFields = ['ID', 'Age', 'Workclass_ID', 'Education_level_ID', 
+        'Education_num', 'Marital_status_ID', 'Occupation_ID', 'Race_id', 'Sex_ID', 
+        'Capital_gain', 'Capital_loss', 'Hours_week', 'Country_ID', 'Over_50k', 'Workclass', 
+        'Educational_level', 'Marital_status', 'Occupation', 'Relationship', 'Race', 'Sex', 'Country'];
+        // Creo un nuovo oggetto Json2csvParser passandogli i campi
+        const json2csvParser = new Json2csvParser({ csvFields });
+        // Converto i records in formato CSV
+        const csvData = json2csvParser.parse(jsonRecords);
+        // Setto l'header HTTP
+        res.setHeader('Content-disposition', 'attachment; filename=records.csv');
+        res.set('Content-Type', 'text/csv');
+        // Effettuo il download del file CSV
+        res.status(200).end(csvData);
     })
 });
